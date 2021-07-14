4352966abb1c8581530afb32b4cf50d1675e6dee
Tai-Rocha <taina013@gmail.com>
2021-07-14 00:23:07 -0300

    I`m tired :hot_face:

--- R script/resume_1.R
+++ R script/resume_1.R
@@ -6,7 +6,7 @@ library(googlesheets4)
 datadrivencv::use_datadriven_cv(full_name = "Tainá Rocha",
 data_location = "https://docs.google.com/spreadsheets/d/1MUKWLJtWwxwiAsSwOghy1tkq41rTp9Ivt6y3LVx2JTI/edit#gid=917338460",
 pdf_location = "https://github.com/Tai-Rocha/Tai-Rocha_CV.github.io/raw/main/Teste_datadriven.pdf",
-html_location = "Tai-Rocha.me/Tai-Rocha_CV.github.io/",
+html_location = "https://github.com/Tai-Rocha/Tai-Rocha_CV.github.io/",
 source_location = "https://github.com/Tai-Rocha/Tai-Rocha_CV.github.io/"
 )
 
